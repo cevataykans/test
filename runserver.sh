a3a69ed1ee536585cd0f6a65704be35da803051c
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-16 19:06:18 +0100

    Test local conenctivity

--- runserver.sh
+++ runserver.sh
@@ -8,4 +8,4 @@ fi
 echo "checking for updates ..."
 /usr/games/steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir /home/abiotic/abioticserver +login anonymous +app_update 2857200 +quit
 echo "Launching server ...." 
-WINEDEBUG=fixme-all xvfb-run wine64 abioticserver/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -NoAsyncLoadingThread -MaxServerPlayers=6 -PORT=7777 -QUERYPORT=27015 -tcp -ServerPassword=cevat -SteamServerName="CevatServer" -WorldSaveName="CevatWorld"
+WINEDEBUG=fixme-all xvfb-run wine64 abioticserver/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -UseLocalIPs -MaxServerPlayers=12 -PORT=7777 -QUERYPORT=27015 -tcp -ServerPassword=cevat -SteamServerName="Acevat Server" -WorldSaveName="CevatWorld"
