72eed70c12edc5688bb914406bfd76d8b7c2609f
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-16 17:10:27 +0100

    Test server hosting with unique filtering

--- runserver.sh
+++ runserver.sh
@@ -5,4 +5,7 @@ if [ ! -d "/home/abiotic/.wine" ]; then
     wineboot --init
 fi
 
-xvfb-run wine64 abioticserver/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -NoAsyncLoadingThread -MaxServerPlayers=6 -PORT=7777 -QUERYPORT=27015 -tcp -ServerPassword=dedicatedtest -SteamServerName="AbioticKAL" -WorldSaveName="Cascade"
+echo "checking for updates ..."
+/usr/games/steamcmd +@sSteamCmdForcePlatformType windows +force_install_dir /home/abiotic/abioticserver +login anonymous +app_update 2857200 +quit
+echo "Launching server ...." 
+WINEDEBUG=fixme-all xvfb-run wine64 abioticserver/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -NoAsyncLoadingThread -MaxServerPlayers=6 -PORT=7777 -QUERYPORT=27015 -tcp -ServerPassword=cevat -SteamServerName="CevatServer" -WorldSaveName="CevatWorld"
