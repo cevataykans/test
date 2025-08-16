cc1c95ccd39b800d6a481836f268ef19e908a0af
Cevat Aykan Sevinc <aykansevinc66@gmail.com>
2025-08-16 16:29:28 +0100

    Add wineboot

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Initialize Wine prefix if needed
+if [ ! -d "/home/abiotic/.wine" ]; then
+    wineboot --init
+fi
+
 xvfb-run wine64 abioticserver/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -NoAsyncLoadingThread -MaxServerPlayers=6 -PORT=7777 -QUERYPORT=27015 -tcp -ServerPassword=dedicatedtest -SteamServerName="AbioticKAL" -WorldSaveName="Cascade"
